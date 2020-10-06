f98028d67fdb6b5fb7c98b124df02f44c564f876
Javier Curto <curto.brull.javier@gmail.com>
2020-10-06 23:47:52 +0200

    fin de curso

--- java_curso/src/java_curso/Arrays.java
+++ java_curso/src/java_curso/Arrays.java
@@ -26,11 +26,25 @@ public class Arrays {
         androidVersions[1] = "Bannana Bread";
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
+
+        // de 0 a N-1 o poner < xxx.length / Valor por defecto de un String es null
+        for (int i = 0; i < androidVersions.length ; i++) {
+            System.out.println(androidVersions[i]);
+        }
+        System.out.println();
+        System.out.println();
+
+        for (String androidVersion : androidVersions) {
+            System.out.println(androidVersion);
+        }
+        for (int i = 0; i <= 3 ; i++) {
+            System.out.println(androidVersions[i]);
+        }
         
-        System.out.println(androidVersions[0]);
+        /*System.out.println(androidVersions[0]);
         System.out.println(androidVersions[1]);
         System.out.println(androidVersions[2]);
-        System.out.println(androidVersions[3]);
+        System.out.println(androidVersions[3])*/;
         
         System.out.println("");
         System.out.println("");
@@ -43,26 +57,60 @@ public class Arrays {
         cities[2][1] = "Guadalajara";
         cities[3][0] = "México";
         cities[3][1] = "CDMX";
-        
-        System.out.println(cities[0][0]);
+
+        // 2 Dimensiones 2 for
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+        System.out.println();
+        System.out.println();
+
+        for (String[] parejas : cities) {
+            for (String name : parejas) {
+                System.out.println(name);
+            }
+        }
+
+        /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
         
         System.out.println("");
         System.out.println("");
         
         String[][][][] animals = new String[2][3][2][2];
         animals[1][0][0][1] = "Chango";
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 0; j++) {
+                for (int k = 0; k <= 0; k++) {
+                    for (int l = 0; l <= 1; l++) {
+                        System.out.println(animals[i][j][k][l]);
+                    }
+                }
+            }
+        }
         
-        System.out.println(animals[1][0][0][1]);
+//        System.out.println(animals[1][0][0][1]);
         
         System.out.println("");
         System.out.println("");
+
+        int x = 1; while (x <= 10) System.out.println(++x);
+        String arreglo[] = new String[10];
+        arreglo[0] = "hola";
+        System.out.println(arreglo[0]);
+        int i=1, j=2, k=3, m=2; System.out.println ((j >= i) || (k == m));
+//        Foo f[] = new Foo[20];
+        byte edad = 24;
+        System.out.println(edad);
     }
     
 }
